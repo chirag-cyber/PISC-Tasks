10ba9a5d16c65303f432e0f9fc94a2b3c03a1ba4
chirag-cyber <chirag1088.be20@chitkarauniversity.edu.in>
2024-02-15 18:37:18 +0530

    commit new-Component

--- Task01/src/App.jsx
+++ Task01/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import 'reactjs-popup/dist/index.css';
 import './App.css';
 import PopupComponent from "../src/Components/Modal";
+import Weather from "./Components/Weather";
 function App() {
 
   const [name,setName] = useState('');
@@ -9,6 +10,7 @@ function App() {
     <div>
     <input type="text" placeholder="Enter your name"  className="input1" value={name} onChange={(e) => setName(e.target.value)}/>
     <PopupComponent name={name} onClose = {() => setName('')} position={"right center"}/>
+    <Weather />
     </div>
   )
 }
